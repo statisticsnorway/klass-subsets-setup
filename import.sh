499f939ebf7d9e4ef9cf736c0b4e1ff639ec0b52
Olve Drageset <olve.drageset@gmail.com>
2020-05-13 11:49:28 +0200

    echo which jq we are using

--- import.sh
+++ import.sh
@@ -2,12 +2,14 @@
 
 if [ ! -d "jq-win64.exe" ];
 then
+  echo "using jq on PATH"
   for i in $(find examples -type f); do
     ENTITY=$(echo "$i" | sed 's:examples/::' | sed 's:\([^_]*\).*:\1:')
     ID=$(jq -r .id "$i")
     curl -i -X PUT http://localhost:9090/ns/${ENTITY}/${ID} --data-binary "@$i" &
   done
 else
+  echo "using jq-win64.exe"
   for i in $(find examples -type f); do
     ENTITY=$(echo "$i" | sed 's:examples/::' | sed 's:\([^_]*\).*:\1:')
     ID=$(./jq-win64.exe -r .id "$i")
