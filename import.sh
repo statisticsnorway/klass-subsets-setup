ab679a78f8f64e7b1b29c05903cf5720324d48c3
Olve Drageset <olve.drageset@gmail.com>
2020-05-13 10:39:50 +0200

    added option to use jq-win64.exe or jq installed with package manager

--- import.sh
+++ import.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-for i in $(find examples -type f); do
-  ENTITY=$(echo "$i" | sed 's:examples/::' | sed 's:\([^_]*\).*:\1:')
-  ID=$(./jq-win64.exe -r .id "$i")
-  curl -i -X PUT http://localhost:9090/ns/${ENTITY}/${ID} --data-binary "@$i" &
-done
-
-wait
+if [ ! -d "jq-win64.exe" ];
+then
+  for i in $(find examples -type f); do
+    ENTITY=$(echo "$i" | sed 's:examples/::' | sed 's:\([^_]*\).*:\1:')
+    ID=$(jq -r .id "$i")
+    curl -i -X PUT http://localhost:9090/ns/${ENTITY}/${ID} --data-binary "@$i" &
+  done
+else
+  for i in $(find examples -type f); do
+    ENTITY=$(echo "$i" | sed 's:examples/::' | sed 's:\([^_]*\).*:\1:')
+    ID=$(./jq-win64.exe -r .id "$i")
+    curl -i -X PUT http://localhost:9090/ns/${ENTITY}/${ID} --data-binary "@$i" &
+  done
+fi
