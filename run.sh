f8b926a8da0c911743abeeb3df83be3cbc6833d2
Olve Drageset <olve.drageset@gmail.com>
2020-05-13 13:02:22 +0200

    check for existence of graphql file

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ fi
 
 if [ ! -d "gsim-raml-schema/graphqlschemas" ]; then
   docker run -v "$1"/gsim-raml-schema:/raml-project statisticsnorway/raml-to-graphql-schema:latest
+elif [ ! -f "gsim-raml-schema/graphqlschemas/schema.graphql" ]; then
+  docker run -v "$1"/gsim-raml-schema:/raml-project statisticsnorway/raml-to-graphql-schema:latest
 fi
 
 docker-compose up -d
