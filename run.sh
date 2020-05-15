ce0ca67b618fc5b7f023874b3bbbfc1b45a43a54
Olve Drageset <olve.drageset@gmail.com>
2020-05-15 11:44:32 +0200

    cleaned code run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,7 @@ if [ ! -d "gsim-raml-schema" ]; then
   git clone https://github.com/statisticsnorway/gsim-raml-schema.git
 fi
 
-if [ ! -d "gsim-raml-schema/graphqlschemas" ]; then
-  docker run -v "$1"/gsim-raml-schema:/raml-project statisticsnorway/raml-to-graphql-schema:latest
-elif [ ! -f "gsim-raml-schema/graphqlschemas/schema.graphql" ]; then
+if [ ! -d "gsim-raml-schema/graphqlschemas" ] || [ ! -f "gsim-raml-schema/graphqlschemas/schema.graphql" ]; then
   docker run -v "$1"/gsim-raml-schema:/raml-project statisticsnorway/raml-to-graphql-schema:latest
 fi
 
