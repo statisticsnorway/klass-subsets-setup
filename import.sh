ebad41413bfdd033e252a0ab9f484ad38ccde4c1
Olve Drageset <olve.drageset@gmail.com>
2020-05-15 11:44:17 +0200

    cleaned code import.sh

--- import.sh
+++ import.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
 
+jqv=-1
 if output=$(jq --help);
 then
-  echo "using jq on PATH"
-  for i in $(find examples -type f); do
-    ENTITY=$(echo "$i" | sed 's:examples/::' | sed 's:\([^_]*\).*:\1:')
-    ID=$(jq -r .id "$i")
-    curl -X PUT http://localhost:9090/ns/"${ENTITY}"/"${ID}" -d "@$i" -H "Content-Type: application/json"
-  done
+  jqv=jq
 elif [ -f "jq-win64.exe" ];
 then
-  echo "using jq-win64.exe"
-  for i in $(find examples -type f); do
-    ENTITY=$(echo "$i" | sed 's:examples/::' | sed 's:\([^_]*\).*:\1:')
-    ID=$(./jq-win64.exe -r .id "$i")
-    curl -X PUT http://localhost:9090/ns/"${ENTITY}"/"${ID}" -d "@$i" -H "Content-Type: application/json"
-  done
+  jqv=./jq-win64.exe
 else
   echo "FAILED: 'jq' is not on PATH nor found locally in project folder as 'jq-win64.exe'."
 fi
+
+for i in $(find examples -type f); do
+  ENTITY=$(echo "$i" | sed 's:examples/::' | sed 's:\([^_]*\).*:\1:')
+  ID=$("$jqv" -r .id "$i")
+  curl -X PUT http://localhost:9090/ns/"${ENTITY}"/"${ID}" -d "@$i" -H "Content-Type: application/json"
+done
 
 wait
