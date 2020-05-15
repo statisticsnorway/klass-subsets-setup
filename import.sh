25aa479194e7c1f654c06fc1ce6d4c6f43c44640
mmj <mmj@ssb.no>
2020-05-15 09:02:30 +0200

    Linked Data Store works now

--- import.sh
+++ import.sh
@@ -3,7 +3,7 @@
 for i in $(find examples -type f); do
   ENTITY=$(echo "$i" | sed 's:examples/::' | sed 's:\([^_]*\).*:\1:')
   ID=$(./jq-win64.exe -r .id "$i")
-  curl -i -X PUT http://localhost:9090/ns/${ENTITY}/${ID} --data-binary "@$i" &
+  curl -X PUT http://localhost:9090/ns/"${ENTITY}"/"${ID}" -d "@$i" -H "Content-Type: application/json"
 done
 
 wait
