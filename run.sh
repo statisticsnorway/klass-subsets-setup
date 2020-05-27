508abfe5ea69a5f8adb24aebe040e234cbd77ad6
Olve Drageset <olve.drageset@gmail.com>
2020-05-27 11:57:00 +0200

    use latest lds server

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@ if [ ! -f "gsim-raml-schema/graphqlschemas/schema.graphql" ]; then
   docker run -v "$1"/gsim-raml-schema:/raml-project statisticsnorway/raml-to-graphql-schema:latest
 fi
 
+docker pull statisticsnorway/lds-server:latest
 docker-compose up -d
