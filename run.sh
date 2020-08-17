fa38f1862e6c0c126743e0a5ad4a928f958a75b9
Olve Drageset <olve.drageset@gmail.com>
2020-08-17 15:12:44 +0200

    don't keep gsim-raml-schemas repo, only the final graphql schemas

--- run.sh
+++ run.sh
@@ -2,13 +2,16 @@
 
 docker-compose down
 
-if [ ! -d "gsim-raml-schema" ]; then
-  git clone https://github.com/statisticsnorway/gsim-raml-schema.git
-fi
+
+rm -rf gsim-raml-schema
+git clone https://github.com/statisticsnorway/gsim-raml-schema.git
 
 if [ ! -f "gsim-raml-schema/graphqlschemas/schema.graphql" ]; then
   docker run -v "$1"/gsim-raml-schema:/raml-project --rm statisticsnorway/raml-to-graphql-schema:latest
 fi
+
+cp -r gsim-raml-schema/graphqlschemas .
+rm -rf gsim-raml-schema
 
 docker pull statisticsnorway/lds-server:1.0.9
 docker-compose up -d
