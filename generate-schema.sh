5afaca760010ca0ca81e020f32d057cadf83da2f
Olve Drageset <olve.drageset@gmail.com>
2020-12-07 09:22:48 +0100

    take parameter for gsim raml schema folder

--- generate-schema.sh
+++ generate-schema.sh
@@ -7,5 +7,5 @@ if [ ! -f "gsim-raml-schema/graphqlschemas/schema.graphql" ]; then
   docker run -v "$1"/gsim-raml-schema:/raml-project --rm statisticsnorway/raml-to-graphql-schema:latest
 fi
 
-cp -r gsim-raml-schema/graphqlschemas .
+cp -r "$1"/gsim-raml-schema/graphqlschemas .
 rm -rf gsim-raml-schema
